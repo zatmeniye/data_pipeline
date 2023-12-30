e414ff26b44c2743d7a01dbe0164085fff48f0e9
zatmeniye <zatmeniye.zatmeniye@gmail.com>
2023-12-30 22:11:57 +0700

    добавлен пример bi в docker compose

--- bi/internal/app/app.go
+++ bi/internal/app/app.go
@@ -2,12 +2,15 @@ package app
 
 import (
 	"bi/config"
+	"bi/internal/controller/http"
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 )
 
 func Run(l *zap.SugaredLogger, cfg *config.Config) error {
 	app := fiber.New()
+
+	http.WithRouter(l, app)
 
 	return app.Listen(cfg.Http.Address)
 }
---
+++ bi/internal/controller/http/router.go
@@ -0,0 +1,12 @@
+package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+func WithRouter(
+	l *zap.SugaredLogger,
+	app *fiber.App,
+) {
+}
